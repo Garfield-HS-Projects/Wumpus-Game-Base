03a4d5b0d93eff97bbb2dac40e38ede244f623f2
Jordan Naftolin <jordann@zillowgroup.com>
2020-03-12 14:29:41 -0700

    The base game

---
+++ Htw/Htw/forms/MainMenuForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace wumpus.forms
+{
+    partial class MainMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(121, 65);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(305, 40);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "Hunt the Wumpus";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(208, 252);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(131, 68);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.BackColor = System.Drawing.Color.Blue;
+            this.startGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startGameButton.ForeColor = System.Drawing.Color.White;
+            this.startGameButton.Location = new System.Drawing.Point(155, 138);
+            this.startGameButton.Margin = new System.Windows.Forms.Padding(5);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(237, 81);
+            this.startGameButton.TabIndex = 4;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseVisualStyleBackColor = false;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 445);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startGameButton);
+            this.Name = "MainMenuForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button startGameButton;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Wumpus-Game-Base
-The game “foundation” (i.e. the solution file, the project file, a mainMenu form, and example component). We can then fork this repo for each of the teams so that they have what they need to get started.
+# Hunt the Wumpus
+This is our implementation of the Hunt the Wumpus game!
+
+
+## Code
+Under the Htw directory is the Visual Studio solution file, along with the Htw project and the HtwTests project.
+
+### The Htw project
+The Htw project is broken up into different packages
+
+#### common
+This contains classes that are common across all objects, like the Direction enumeration (which needs to be filled in)
+
+#### components
+This contains the objects that you're assigned to (e.g. Cave, GameControl, etc.). 
+Initially, it just contains an example component class.
+
+#### forms
+This contains any User Interface forms. 
+Initially, it just contains the MainMenuForm.
+
+#### Properties
+This is where the game's embedded resources live (like images and sounds)
+
+
+### The HtwTests project
+The HtwTests allow you to make sure that your object is working as you expect. 
+You'll write tests to validate your object's behavior.
+Initially, it just contains an example test that create the Example component and validates the result of the Initialize method call.
+
+## Samples
+The samples directory contains some sample code to help develop the game
